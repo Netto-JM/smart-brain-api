d3a534f43ce9c02f1b1d2e77a07b6a5580d5bfcb
Netto-JM <32754552+Netto-JM@users.noreply.github.com>
2021-06-08 00:30:17 -0300

    adding connections

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ import { handleImage, handleApiCall } from './controllers/image.js';
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+  }
   }
 });
 
